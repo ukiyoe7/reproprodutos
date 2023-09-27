7aaad214bbe035d3e1478d8ab973f0769d576a4b
Sandro Jk <sandro007@gmail.com>
2023-09-27 12:57:56 -0300

    ok

--- EXCECAO.R
+++ EXCECAO.R
@@ -4,14 +4,80 @@ library(DBI)
 library(tidyverse)
 library(lubridate)
 library(googlesheets4)
-con2 <- dbConnect(odbc::odbc(), "reproreplica")
+con2 <- dbConnect(odbc::odbc(), "reproreplica",encoding = "latin1")
+
+linhas_ativas <-
+dbGetQuery(con2,"SELECT PROCODIGO,PRODESCRICAO,
+           TPLCODIGO FROM PRODU WHERE
+           GR1CODIGO<>17 AND PROSITUACAO='A'
+                 AND PRODESCRICAO LIKE '%XR%'") %>% 
+  mutate(PROCODIGO=str_trim(PROCODIGO))
+  
+  
+col_ativos2 <-
+  dbGetQuery(con2,"SELECT PROCODIGO,PRODESCRICAO,
+           TPLCODIGO FROM PRODU WHERE
+           GR1CODIGO<>17 AND PROSITUACAO='A'
+                 AND PRODESCRICAO LIKE '%COLOR%' AND PROTIPO IN ('C','X')") %>%
+  mutate(PROCODIGO=str_trim(PROCODIGO))
 
 
-tpdbGetQuery(con2,"SELECT * FROM TPLPRO WHERE PROCODIGO='TRHCT2'") 
+crossjoin <-
+merge(linhas_ativas,col_ativos2,by=NULL) 
+  
+  
+  write.csv2(crossjoin,row.names=FALSE,na="",file ="C:\\Users\\Repro\\Documents\\PRODUTOS\\2023\\AGO\\col2.csv")
 
-dbGetQuery(con2,"SELECT * FROM TPLPRO WHERE TPLCODIGO=4418") %>% View()
+  
 
-dbGetQuery(con2,"SELECT PROCODIGO,TPLCODIGO FROM PRODU WHERE  ") %>% View()
+tplpro <- 
+dbGetQuery(con2,"
+  WITH PROD AS (SELECT PROCODIGO PROCODIGO_LINHA,PRODESCRICAO,TPLCODIGO FROM PRODU WHERE PROSITUACAO='A' AND PRODESCRICAO LIKE '%XR%'),
+  
+  PROD2 AS (SELECT PROCODIGO ,PRODESCRICAO PRODESCRICAO_SERV FROM PRODU WHERE PROSITUACAO='A')
+  
+  SELECT T.PROCODIGO,PRODESCRICAO_SERV,PROCODIGO_LINHA,PRODESCRICAO,T.TPLCODIGO FROM TPLPRO T
+             INNER JOIN PROD P ON P.TPLCODIGO=T.TPLCODIGO
+             LEFT JOIN PROD2 P2 ON P2.PROCODIGO=T.PROCODIGO
+           ") %>% mutate(PROCODIGO=str_trim(PROCODIGO)) %>% mutate(PROCODIGO_LINHA=str_trim(PROCODIGO_LINHA))
+  View(tplpro )
+
+
+write.csv2(tplpro,row.names=FALSE,na="",file ="C:\\Users\\Repro\\Documents\\PRODUTOS\\2023\\SET\\tplpro.csv")
+
+
+dbGetQuery(con2,"SELECT PROCODIGO,TPLCODIGO FROM PRODU WHERE PROCODIGO='LP0182' ") %>% View()
+
+
+
+dbGetQuery(con2,"SELECT PROCODIGO,TPLCODIGO FROM PRODU WHERE PROCODIGO LIKE '%100%' ") %>% View()
+
+tlppro_xr <-
+inner_join(tplpro,linhas_ativas,by="TPLCODIGO") %>% filter(str_detect(PROCODIGO.x,"100")) %>% select(TPLCODIGO,PROCODIGO.x) %>% rename(PROCODIGO=2)
+
+anti_join(linhas_ativas,
+inner_join(tplpro %>% filter(str_detect(PROCODIGO,"COLEXT ")),linhas_ativas,by=c("TPLCODIGO")),by="TPLCODIGO") %>% View()
+
+
+
+
+@#CHARACTER#PRODUTOS@
+
+excecao <- dbGetQuery(con2,"
+           SELECT DISTINCT T.TPLCODIGO LINHA,T.PROCODIGO,PRODESCRICAO FROM TPLPRO T
+          INNER JOIN (SELECT PROCODIGO,PRODESCRICAO,TPLCODIGO FROM PRODU
+          
+          ) A ON T.PROCODIGO=A.PROCODIGO") 
+
+
+range_write("1zNDds9P36i3PMUSCQ6BNyhT06mXfZ48NMnkfsf6iKJQ",data=excecao,sheet = "DADOS",
+            range = "A1",reformat = FALSE) 
+
+dbGetQuery(con2,"SELECT * FROM TPLCLI WHERE CLICODIGO=151 ") %>% View()
+
+
+
+
 
 
 ## VERIFICA LINHAS COM EXCEÇOES
@@ -33,7 +99,9 @@ produto <- dbGetQuery(con2,"
 View(produto)
 
 
-excecao <- dbGetQuery(con2,"SELECT * FROM TPLPRO WHERE PROCODIGO='COLEXT'") 
+excecao <- dbGetQuery(con2,"SELECT * FROM TPLPRO WHERE PROCODIGO='MOEST'") 
+
+View(excecao)
 
 left_join(produto,excecao,by="TPLCODIGO") %>% View()
 
@@ -52,6 +120,14 @@ for (i in 1:nrow(insert_excecao)) {
 }
 
 
-query1 <- paste("DELETE FROM TPLPRO WHERE TPLCODIGO=3724 AND PROCODIGO='TRHCT'")
+query1 <- paste("DELETE FROM TPLPRO WHERE TPLCODIGO=2377 AND PROCODIGO='CODC2UVT'")
 dbSendQuery(con3,query1)
+
+
+
+
+query8 <- paste("DELETE FROM PRODUEXCPRODU WHERE PROCODIGO='COCAM' AND PROCODIGOEXC='TRFAS'")
+dbSendQuery(con3,query8)
+
+
 
